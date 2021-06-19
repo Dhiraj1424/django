dacaf18087ad05bd8e08c91e06769cd56b9e2653
Dhiraj Subedi <dhirajsubedi924@gmail.com>
2021-06-19 06:48:26 +0545

    logout and login

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from .models import *
 from .forms import OrderForm
 from .forms import CreateUserForm
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 
 # Create your views here.
@@ -38,8 +39,23 @@ def registerPage(request):
 
 
 def loginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'Username or Password is incorrect')
+
+    
     context = {}
     return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 def home(request):
